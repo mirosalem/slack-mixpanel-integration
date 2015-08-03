ae06248011149ffe6a3944e5ecc311e249ccb1af
toshipon <toshipon123@gmail.com>
2015-08-04 00:40:06 +0900

    changed color

--- plugins/mixpanel/mixpanel.php
+++ plugins/mixpanel/mixpanel.php
@@ -7,26 +7,26 @@
 		const DEFAULT_BOT_NAME = "Mixpanel";
 
 		function onHook($request){
-			// $payload = $request['post'];
-			// if (!$payload || !is_array($payload)) return array('ok' => false, 'error' => "invalid_payload");
-      //
-			// $text = '';
-			// if (isset($payload['users']))
-			// {
-			// 	$users = json_decode($payload['users'], true);
-			// 	if (!$users) return array('ok' => false, 'error' => "invalid_payload");
-      //
-			// 	foreach($users as $user) {
-			// 		$text = $user['$email'] . ' \n';
-			// 	}
-			// }
+			$payload = $request['post'];
+			if (!$payload || !is_array($payload)) return array('ok' => false, 'error' => "invalid_payload");
 
-      $text = 'This is test.';
+			$text = '';
+			if (isset($payload['users']))
+			{
+				$users = json_decode($payload['users'], true);
+				if (!$users) return array('ok' => false, 'error' => "invalid_payload");
+
+				foreach($users as $user) {
+					$text = $user['$email'] . ' \n';
+				}
+			}
+
+      // $text = 'This is test.';
 
 			$attachment = array(
 				'text' 		=> $text,
 				'fallback'	=> 'Mixpanel notification',
-				'color' 	=> '9B59B6',
+				'color' 	=> '647997',
 				'mrkdwn_in'	=> 'text',
 			);
 
